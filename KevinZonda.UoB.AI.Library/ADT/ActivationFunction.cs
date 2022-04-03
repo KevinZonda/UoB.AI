e22eafbb64c7dd354ea39d5aaff3da016d870c9c
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-04-03 11:33:27 +0100

    chore: extense Activ. func

--- KevinZonda.UoB.AI.Library/ADT/ActivationFunction.cs
+++ KevinZonda.UoB.AI.Library/ADT/ActivationFunction.cs
@@ -9,18 +9,20 @@ public abstract class ActivationFunction
         return GetLabelByResult(Calculate(input));
     }
 
-    public static ActivationLabel GetLabelByResult(double calcResult)
+    public virtual ActivationLabel GetLabelByResult(double calcResult)
     {
         return calcResult switch
         {
             double d when d < 0 || d > 1 => throw new ArgumentOutOfRangeException(nameof(calcResult),
                 "Input must be between 0 and 1"),
-            double d when d == 0.5 => ActivationLabel.DecisionBoundary,
-            double d when d < 0.5 => ActivationLabel.Label0,
-            double d when d > 0.5 => ActivationLabel.Label1,
+            double d when d == DecisionBoundary => ActivationLabel.DecisionBoundary,
+            double d when d < DecisionBoundary => ActivationLabel.Label0,
+            double d when d > DecisionBoundary => ActivationLabel.Label1,
             _ => throw new NotImplementedException()
         };
     }
+
+    public virtual double DecisionBoundary { get; protected set; } = 0.5;
 }
 
 public enum ActivationLabel
