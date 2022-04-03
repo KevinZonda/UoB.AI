6d8f31bf5612a35a25f2c4a0e31fa74ce8a95aee
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-04-03 08:53:16 +0100

    feat: vectorlise G.D.

--- KevinZonda.UoB.AI.Library/Mathematics/Functions/GradientDescent.cs
+++ KevinZonda.UoB.AI.Library/Mathematics/Functions/GradientDescent.cs
@@ -1,28 +1,38 @@
 ﻿using KevinZonda.UoB.AI.Library.ADT;
+using KevinZonda.UoB.AI.Library.Data;
+using KevinZonda.UoB.AI.Library.Data.Extension;
 using KevinZonda.UoB.AI.Library.Mathematics.Operations;
 
 namespace KevinZonda.UoB.AI.Library.Mathematics.Functions;
 
 public class GradientDescent
 {
-    public static double[] FindMinimum(double[] initial, double stepSize, double tolerance,
-        Func<double[], double[]> nabla, DistanceFunction distance, int maxIterations = int.MaxValue)
+    public static double[] FindMinimum(
+        Vector<double> initial, double stepSize, double tolerance,
+        Func<Vector<double>, Vector<double>> nabla,
+        DistanceFunction distance, int maxIterations = int.MaxValue)
     {
         var current = initial;
         var previous = initial;
 
         for (var i = 0; i < maxIterations; i++)
         {
-            var gradient = nabla(current);
-            var direction = stepSize.Multiply(gradient);
-            var newPoint = current.Add(direction);
+            var newPoint= OneStep(current, stepSize, nabla(current));
 
-            if (distance.CalculateDistance(newPoint, previous) < tolerance) return newPoint;
+            if (distance.CalculateDistance(newPoint, previous) < tolerance)
+                return newPoint;
 
             previous = current;
             current = newPoint;
         }
 
         return current;
+    }
+
+    public static Vector<double> OneStep(Vector<double> initStep, double stepSize,
+        Vector<double> nabla)
+    {
+        var direction = stepSize.Multiply(nabla);
+        return initStep.Minus(direction.ToVector());
     }
 }
